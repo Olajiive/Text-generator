e0efdfa2cfd08b60845acf5d3ce189775b37d4b2
Olajiive <muizolatunji29@gmail.com>
2022-08-20 06:31:37 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+//lorem
+const text = [
+    `dolor sit amet, consectetur adipisicing elit. Enim laudantium excepturi voluptatibus reiciendis aliquam. Harum placeat, illo quis vero quia assumenda animi perspiciatis at magni nostrum consectetur aspernatur repudiandae! Nostrum.
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem vero mollitia labore nostrum fuga, tenetur amet velit dolores beatae animi rerum totam. Quaerat quos debitis delectus magni nulla fugit architecto.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt consequatur enim quisquam nobis tempore error voluptatem ea, ipsa magni, veniam laborum, qui nulla odit vitae ipsam. Excepturi sapiente odit deleniti.
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo facilis, minima consectetur fugiat aliquam distinctio amet in maxime harum explicabo adipisci quo quidem recusandae natus optio, sequi porro eos delectus.`,
+    `Lorem ipsum or sit, amet consectetur adipisicing elit. Quos libero sapiente eos. Consectetur, impedit! Illum debitis quam, accusamus temporibus iure sunt explicabo reiciendis aliquam aspernatur! Natus nam temporibus tenetur fuga.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat atque ut quibusdam culpa non molestias enim quis corporis aut veritatis. Quibusdam unde odit numquam labore laborum? Consequuntur repellendus reiciendis temporibus?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dicta ad impedit distinctio ipsam alias architecto quasi est ea veniam? Et voluptatum aliquid suscipit minus, debitis qui quam reiciendis blanditiis.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut asperiores maiores quasi itaque labore, facere quas eum aliquid accusantium eaque voluptate voluptatum sapiente veniam quisquam repudiandae. Expedita a doloremque voluptatibus!`,
+    `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas alias recusandae temporibus ullam, veniam aut nostrum ratione illo dolore dicta ducimus totam amet, nam aspernatur eveniet suscipit mollitia, omnis ipsum.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi placeat magni ipsa nesciunt provident culpa suscipit architecto, non facere assumenda mollitia fugiat, reprehenderit qui sed. Totam maxime deserunt aliquam labore.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum accusantium officiis ipsum dolorem unde praesentium delectus eos ipsam quos eveniet veritatis saepe, eaque est harum libero illo? Aliquam, ab incidunt.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt eligendi ut libero esse rem sint eos, soluta laboriosam ex magni reprehenderit pariatur nobis! Voluptate impedit sit aliquam ipsam deleniti nesciunt.`,
+    `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque dolores nisi ea, amet corrupti quae consequuntur odio harum. Neque enim ipsa nam numquam dicta quis illum quos nisi ex porro.
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum, quae. Repudiandae voluptate architecto ipsa dignissimos officia delectus doloremque iste facilis. Molestiae, reiciendis modi. Quos dicta culpa at consectetur ex odio!
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, exercitationem maxime! Nihil inventore dolores quibusdam sequi et doloremque beatae tempore cum soluta, obcaecati, quaerat quas a eveniet corrupti dolore consequuntur?
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur, sequi iste rerum magni vel minima alias nihil numquam neque unde quia, eveniet doloribus ipsum quae ut? Corrupti sint laudantium ullam.`,
+   `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere ex cupiditate quos repudiandae vel, atque aliquam officia possimus modi delectus dolorem quia velit doloribus voluptatibus eum! Dolor sapiente nisi nostrum.
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum atque id, fugiat tempora quam et aspernatur. Ipsam voluptate iure atque, eaque aperiam illum eos maiores quaerat, itaque magnam quos perspiciatis!
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio delectus doloribus, vero asperiores repudiandae et repellat quam necessitatibus aut quas, deleniti perferendis dolore debitis laboriosam deserunt ratione earum voluptatibus consequuntur!
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta esse unde, quibusdam, quidem nemo, exercitationem corporis voluptatem odio vero quo modi ullam? Ipsum quibusdam ex possimus veritatis deleniti ad ratione!`,
+    `Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia obcaecati tenetur esse, velit animi recusandae quas consequuntur ratione alias cumque maiores nemo, debitis doloribus quisquam soluta rem neque totam quam!
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe voluptates pariatur fuga consequuntur vel dolorem ut qui, veritatis non laborum earum aperiam atque magnam libero beatae. Quibusdam quae debitis iusto?
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae accusamus perferendis sequi inventore reiciendis quam sed recusandae tempore et enim beatae ab voluptates illo iste minus, debitis voluptatem! Iusto, similique.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti similique doloremque architecto id magni officia saepe porro explicabo repellendus odit, culpa ex vero commodi ipsum laboriosam. Quam libero odit distinctio?`,
+    `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium, culpa vero, maxime vel sint, esse dolores commodi error porro incidunt eum fuga iste veniam ipsa placeat ipsam voluptatibus consectetur earum.
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo minus asperiores fugit labore fugiat eveniet quam nam adipisci. Neque quasi labore similique, commodi iure doloremque est laborum libero odit dolor?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam qui esse odio explicabo fuga illum autem, nostrum eaque, doloribus recusandae impedit natus numquam. Nisi provident esse obcaecati maxime dicta alias!
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora quidem optio non, delectus rerum ex. Soluta tempora ad quia maiores, ratione odit cupiditate molestias error consequuntur. Omnis iusto incidunt sapiente!`,
+    `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni nesciunt sit beatae fugiat illum ad qui! Error, consectetur vitae recusandae dicta beatae, sint hic voluptates, soluta atque distinctio exercitationem vel.
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio deleniti rerum doloremque libero? Atque laborum id sit, quas at reiciendis voluptate quisquam et magni blanditiis perspiciatis sint beatae nobis earum.
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam, accusantium quas porro facere animi in cupiditate impedit quae, suscipit voluptatum magni corporis! Dolor dolorum, facilis nostrum ipsam voluptas illo vero?
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam atque voluptatem architecto saepe doloribus itaque ullam facilis voluptates autem nemo aut, sapiente iusto sequi beatae sit placeat aspernatur mollitia aperiam?`,
+    `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam impedit et beatae, accusantium facilis id iusto sunt qui saepe numquam, eos mollitia. Delectus eligendi quia odio necessitatibus a eveniet officia!
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque, beatae consequuntur laudantium minima aspernatur quasi autem explicabo aliquid nemo fuga nisi quis atque labore provident, doloribus incidunt quaerat consequatur? Quaerat?
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem tempora eligendi architecto perferendis illum debitis soluta vitae asperiores veniam illo labore eveniet possimus quia ad voluptates porro, ea totam repellendus.
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur laboriosam iure quam debitis porro, delectus temporibus eaque voluptas beatae sint nobis excepturi asperiores necessitatibus hic eum ducimus. Ducimus, velit consectetur?,`
+   
+]
+
+const form = document.querySelector('.lorem-form')
+const amount = document.getElementById('amount')
+const result = document.querySelector('.lorem-text')
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const value = parseInt(amount.value);
+    const random = Math.floor(Math.random()* text.length)
+
+    //empty
+    //-1
+    // > 9
+    if(isNaN(value) || value <= 0 || value > 9){
+        result.innerHTML= `<p class="result">${text[random]}</p>`
+    }
+    else {
+        let tempText = text.slice(0, value);
+        tempText = tempText.map(function (item) {
+            return  `<p class="result">${item}</p>`
+        })
+        .join("")
+        result.innerHTML=tempText
+        
+    }
+    
+})
